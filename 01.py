e107b1227ed5df0118e33369ee6d760fdb7e808b
Chalmersb1073 <100640185+Chalmersb1073@users.noreply.github.com>
2022-03-09 10:19:54 +1300

    Update 01.py

--- 01.py
+++ 01.py
@@ -22,11 +22,20 @@ def num_check(question):
         
 
 
-# main routine
-width = num_check("width: ")
-height = num_check("height: ")
-area = width * height
-perimeter = 2 * (width * height)
-print("perimeter: {} units" .format(perimeter))
-print("area: {} square units" .format(area))
 
+
+# main routine
+
+keep_going = ""
+while keep_going == "":
+
+    width = num_check("width: ")
+    height = num_check("height: ")
+    area = width * height
+    perimeter = 2 * (width * height)
+    print("perimeter: {} units" .format(perimeter))
+    print("area: {} square units" .format(area))
+
+    keep_going = input ("press enter to restart or any key to exit")
+print()
+print("area / perimeter calculator ended")
