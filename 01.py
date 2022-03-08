5a31b9064febad8dbb19680335defbeb8e4ecf3e
Chalmersb1073 <100640185+Chalmersb1073@users.noreply.github.com>
2022-03-09 10:11:31 +1300

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,12 +1,31 @@
-valid = False
-while not valid:
+#functions go here
 
-    response = float(input("enter a number: "))
+def num_check(question):
+    valid = False
+    while not valid:
 
-    if response > 0:
-        valid = True
+        error = "dont be a smartass, pick a positive number"
 
-    else:
-        print("dont be a smartass, pick a positive number")
-        print()
-     
+        try:
+
+            response = float(input(question))
+
+            if response > 0:
+                return response
+
+            else:
+                print(error)
+                print()
+
+        except ValueError:
+            print(error)
+        
+
+
+# main routine
+width = num_check("width: ")
+height = num_check("height: ")
+area = width * height
+perimeter = 2 * (width * height)
+print("perimeter: {} units" .format(perimeter))
+print("area: {} square units" .format(area))
